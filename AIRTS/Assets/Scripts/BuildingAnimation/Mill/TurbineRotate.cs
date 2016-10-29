9a2c68e264a80b31e7ddc1a01f98e72c44dcbc5c
L9Aug <tbampton@hotmail.co.uk>
2016-10-29 23:30:20 +0100

    Added Buildings 2
    1. Farm
    2. Orchard
    3. Plantation
    4. Pasture
    5. Mine
    6. Mill

---
+++ AIRTS/Assets/Scripts/BuildingAnimation/Mill/TurbineRotate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurbineRotate : MonoBehaviour {
+
+    [Range(0, 100)]
+    public float RotationSpeed = 5;
+
+    private float RotationValue = 0;
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        RotationValue = Mathf.Lerp(RotationValue, RotationValue - RotationSpeed, 0.1f);
+        transform.rotation = Quaternion.Euler(0, 0, RotationValue);
+        if (RotationValue < -180) RotationValue += 360;
+    }
+}
