80691f9f3a9a1bf847d3378341f11b81320872cc
L9Aug <tbampton@hotmail.co.uk>
2016-11-11 01:58:54 +0000

    Core building parameters and base scripts created.

---
+++ AIRTS/Assets/Scripts/Buildings/RawProduction.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RawProduction : ProductionBase {
+
+    #region Public Variables
+
+    /// <summary>
+    /// The terrain/s required for this building to perform.
+    /// </summary>
+    [Tooltip("The terrain/s required for this building to perform.")]
+    public List<TerrainTypes> TerrainRequirement = new List<TerrainTypes>();
+
+    #endregion
+
+}
